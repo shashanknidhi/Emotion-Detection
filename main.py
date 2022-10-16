309a673c8fe0584b43d5b702fec0c14a9275e7c3
Shashank Nidhi <shashanknidhi617@gmail.com>
2022-10-16 18:42:06 +0530

    4 files added
    Jupyter notebook, xml, h5 and main py files added.

---
+++ main.py
@@ -0,0 +1,50 @@
+#import libraries
+import cv2
+import numpy as np
+from tensorflow.keras.utils import img_to_array
+import warnings
+warnings.filterwarnings("ignore")
+from keras.models import  load_model
+import numpy as np
+# camera access
+cam = cv2.VideoCapture(0)
+#model load
+model = load_model("best_model.h5")
+#haar cascade classifer to detect face in the frame
+face_haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+while True:
+    ret, test_img = cam.read()  
+    if not ret:
+        continue
+    gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
+
+    detected_face = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+
+    for (x, y, w, h) in detected_face:
+        cv2.rectangle(test_img, (x, y), (x + w, y + h), (255, 0, 0), thickness=4)
+        #pre-processing frames
+        roi_gray = gray_img[y:y + w, x:x + h]
+        roi_gray = cv2.resize(roi_gray, (224, 224))
+        img_pixels = img_to_array(roi_gray)
+        img_pixels = np.expand_dims(img_pixels, axis=0)
+        img_pixels /= 255
+
+        predictions = model.predict(img_pixels)
+
+        # find the label with max probability
+        max_index = np.argmax(predictions[0])
+
+        emotions = ('angry', 'disgust', 'fear', 'happy','neutral', 'sad', 'surprise')
+        predicted_emotion = emotions[max_index]
+
+        cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    resized_img = cv2.resize(test_img, (1000, 700))
+    cv2.imshow('Facial emotion analysis ', resized_img)
+
+    if cv2.waitKey(10) == ord('a'): 
+        break
+
+cam.release()
+cv2.destroyAllWindows
